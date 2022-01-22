2a9b9a6965f691b0bb1447ab49b411c348b23a6e
J-45 <64985480+J-45@users.noreply.github.com>
2022-01-22 01:03:33 +0100

    Update yggd.js

--- yggd.js
+++ yggd.js
@@ -1,3 +1,4 @@
+const proc = require('child_process');
 const fs = require('fs');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth'); // https://github.com/berstend/puppeteer-extra/tree/master/packages/puppeteer-extra-plugin-stealth
@@ -14,33 +15,54 @@ const options = {
     userDataDir: dir,
     headless: false,
     defaultViewport: {
-        width: 1100,
-        height: 1080
+        width: 1100, height: 1080
     }
 };
 
 (async () => {
+    console.log("        -= YGG B0T =-\n");
+    console.log("♪┏(・o･)┛♪┗ ( ･o･) ┓♪┏(・o･)┛\n");
     const browser = await puppeteer.launch(options);
     const [page] = await browser.pages();
+    await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './torrents/'});
     await page.goto('https://yggtorrent.re/');
     await page.waitForTimeout(5000)
-    await page.goto('https://www3.yggtorrent.re/engine/search?name=&do=search');
-    await page.waitForTimeout(1000);
-    const html = await page.evaluate(() => {
-        return document.documentElement.innerHTML;
+
+    proc.exec('aria2c -v', function callback(error, stdout, stderr) {
+        console.log(stdout);
     });
-    console.log('Torrents data:', html.length);
-    const regexp = /<tr>(?:[^>]+>){3}<a href="https:\/\/\w+.yggtorrent(?:[^=]+=){7}"([^"]+)(?:[^>]+>){14}(\d+)(?:[^>]+>){5}([^<]+)<\/td>\s+<td>(\d+)<\/td>\s+<td>(\d+)<\/td>\s+<td>(\d+)<\/td>/gm;
-    const result = [...html.matchAll(regexp)];
-    result.forEach(element => {
-        url = element[1];
-        time = element[2];
-        size = element[3];
-        download = element[4];
-        seeds = element[5];
-        peers = element[6];
-        console.log(`url: ${url}\ntime: ${time}\nsize: ${size}\ndownload: ${download}\nseeds: ${seeds}\npeers: ${peers}\n`);
-    });
+
+    while (true) {
+        await page.goto('https://www3.yggtorrent.re/engine/search?do=search');
+        await page.waitForTimeout(1000);
+        const html = await page.evaluate(() => {
+            return document.documentElement.innerHTML;
+        });
+        const regexp = /<tr>(?:[^>]+>){3}<a href="https:\/\/\w+.yggtorrent(?:[^=]+=){7}"([^"]+)(?:[^>]+>){14}(\d+)(?:[^>]+>){5}([^<]+)<\/td>\s+<td>(\d+)<\/td>\s+<td>(\d+)<\/td>\s+<td>(\d+)<\/td>/gm;
+        const result = [...html.matchAll(regexp)];
+        result.forEach(element => {
+            url = element[1];
+            time = element[2];
+            size = element[3];
+            download = element[4];
+            seeds = element[5];
+            peers = element[6];
+            id = url.match(/https:\/\/\w+.yggtorrent.re\/(?:[^\/]+\/){3}(\d+)-/)[1];
+            torrent_download = "https://www3.yggtorrent.re/engine/download_torrent?id="+id;
+            local_torrent = `./torrents/${id}.torrent`;
+            // if (download < 1 && seeds < 2 && peers < 2 && size.includes("Go")){
+                if (download < 1 && size.includes("Go")){
+                size = parseInt(size.split('Go')[0]);
+                if (size > 1 && size < 10) {
+                    console.log(`url: ${url}\nid: ${id}\ntime: ${time}\nsize: ${size}\ndownload: ${download}\nseeds: ${seeds}\npeers: ${peers}\n`);
+                    proc.exec(`curl -L -C - --url '${torrent_download}' -o ${local_torrent}`, function callback(error, stdout, stderr) {
+                        console.log(stdout);
+                    });
+                }
+            }
+        });
+        await page.waitForTimeout(60000);
+    }
     
     await browser.close();
 })
